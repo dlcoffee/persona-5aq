2e052cf8bee38f9c6744a25c6b1eb5d7b698b02f
daniel tea <daniel.l.tea@gmail.com>
2021-02-20 15:08:45 -0800

    search input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
-export default function Home() {
+export default function Index() {
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const runSearch = async (event) => {
+    event.preventDefault()
+
+    const res = await fetch(`/api/search?input=${search}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
+
+    const response = await res.json()
+    setResults(response)
+  }
+
   return (
     <div>
       <Head>
@@ -8,7 +30,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>main</main>
+      <main>
+        <form onSubmit={runSearch}>
+          <label htmlFor="search">fuzzy search:</label>
+
+          <input
+            type="text"
+            id="search"
+            name="search"
+            value={search}
+            onChange={handleChange}
+          />
+
+          <input type="submit" value="submit" />
+        </form>
+
+        <div>
+          {results.length > 0 && (
+            <ul>
+              {results.map((result, index) => (
+                <li key={index}>
+                  {result.question} | {result.answer}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </main>
 
       <footer>footer</footer>
     </div>
